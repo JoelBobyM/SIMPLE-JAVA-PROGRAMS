5797fe08cc4ed9e1384360515c5aa66253dd828c
VigneshSKumar <92641241+VigneshSKumar@users.noreply.github.com>
2021-10-17 12:05:57 +0530

    Array Add

---
+++ ArrayAdd.java
@@ -0,0 +1,20 @@
+class ArrayAdd
+{
+public static void main(String args[])
+{
+    int a[][]={{1,2},{4,5}};
+    int b[][]={{1,2},{4,5}};
+    int result[][]=new int[2][2];
+
+    for(int i=0;i<2;i++)
+    {
+      for(int j=0;j<2;j++)
+      {
+       result[i][j]=a[i][j]+b[i][j];
+       System.out.println(result[i][j]+" ");
+      }
+   
+    System.out.println();
+   }
+}
+}
